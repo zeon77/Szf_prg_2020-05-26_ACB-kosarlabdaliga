7e3644b0c65a00f022e8fd0a9dea82ef042ad7b6
zeon77 <zeon77@gmail.com>
2021-06-07 08:40:30 +0200

    Add project files.

---
+++ kosar2004/kosar2004.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace kosar2004
+{
+    class kosar2004
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
